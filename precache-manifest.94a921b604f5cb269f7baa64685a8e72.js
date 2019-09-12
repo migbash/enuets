a2bd5bf7d3d2323a07b360232c61ec8cadaf5f23
migbash <20924663+migbash@users.noreply.github.com>
2019-09-12 04:56:33 +0100

    Updates

---
+++ precache-manifest.94a921b604f5cb269f7baa64685a8e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed576adc364fa528ad7227684c7a72a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7b1dadd04d6f977f04f",
+    "url": "./static/css/main.eb728e3c.chunk.css"
+  },
+  {
+    "revision": "e2fae376275623bb3311",
+    "url": "./static/js/2.3b22d1ba.chunk.js"
+  },
+  {
+    "revision": "a7b1dadd04d6f977f04f",
+    "url": "./static/js/main.23be8c66.chunk.js"
+  },
+  {
+    "revision": "3ef4c2384531dcca5d66",
+    "url": "./static/js/runtime~main.6a226697.js"
+  }
+]);
