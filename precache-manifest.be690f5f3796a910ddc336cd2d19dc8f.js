ba1ad5a1fc373c02b067ff001c982ed44c656cdf
migbash <20924663+migbash@users.noreply.github.com>
2019-09-12 05:36:32 +0100

    Updates

---
+++ precache-manifest.be690f5f3796a910ddc336cd2d19dc8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce48017a951a50071c5082fbd4480159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55622a0e13947df47c14",
+    "url": "/static/css/main.11f44543.chunk.css"
+  },
+  {
+    "revision": "e2fae376275623bb3311",
+    "url": "/static/js/2.3b22d1ba.chunk.js"
+  },
+  {
+    "revision": "55622a0e13947df47c14",
+    "url": "/static/js/main.e45362c2.chunk.js"
+  },
+  {
+    "revision": "35065c8bf4b4ca5735c9",
+    "url": "/static/js/runtime~main.d2f596a9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enuets/precache-manifest.559cb162ec4cc248eb7f1ec3c427104b.js"
+  "/precache-manifest.be690f5f3796a910ddc336cd2d19dc8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/enuets/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
